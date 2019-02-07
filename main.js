6ca8107c769c9cfa0f39fdf54f37de9c02273020
RM147 <43373305+RM147@users.noreply.github.com>
2019-02-07 14:25:14 +0000

    new

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 'use strict';
+var box = 0;
+
+function showBox(){
+    if (box>=4){
+        document.getElementById("b1").style.color = "white";
+        document.getElementById("para3").style.color = "mediumblue";
+        document.getElementById("b1").style.backgroundColor = "#4CAF50";       
+    }else if(box%2==0){
+        document.getElementById("para1").style.color = "white";
+        document.getElementById("para2").style.color = "mediumblue";
+    }else if(box==3){
+        document.getElementById("para1").style.color = "mediumblue";
+        document.getElementById("para3").style.color = "white";
+    }
+    else{
+        document.getElementById("para1").style.color = "mediumblue";
+        document.getElementById("para2").style.color = "white";
+    }
+box++;
+}
